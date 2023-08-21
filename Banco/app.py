0f99ba4f6959ef9d89fbc4a2d41f53fb06638d74
nicholasleal0 <contatoluisfernandogaleano@gmail.com>
2023-08-21 17:39:26 -0400

    commit_apdade_banco_mauricio

--- Banco/app.py
+++ Banco/app.py
@@ -1,6 +1,6 @@
 from typing import Optional
-import PySide6.QtCore
-from PySide6.QtWidgets import QMainWindow,QApplication,QPushButton,QLabel,QLayout,QLineEdit,QHBoxLayout,QVBoxLayout,QWidget,QTextBrowser
+import PySide2.QtCore
+from PySide2.QtWidgets import QMainWindow,QApplication,QPushButton,QLabel,QLayout,QLineEdit,QHBoxLayout,QVBoxLayout,QWidget,QTextBrowser,QCheckBox
 import sys
 class Banco(QWidget):
 
@@ -9,23 +9,18 @@ class Banco(QWidget):
         super().__init__()
         self.setWindowTitle("Vulgo-Senacoin")
         self.layout = QVBoxLayout()
-        
-        #Listas Para Guardar os dados 
-        self.list_nome = []
-        self.list_num_conta = []
-        self.list_saldo = []
-
+        self.layout_hor = QHBoxLayout()
 
         #Cadastro Cliente
         self.lbl_nome = QLabel("Nome:")
         self.lbl_numero_conta = QLabel("Número da Conta:")
-        self.lbl_deposito_inicial = QLabel("Deposito Inicial:")
-        self.lbl_saque = QLabel("Digite o valor de saque:")
+        # self.lbl_deposito_inicial = QLabel("Deposito Inicial:")
+        self.lbl_saldo = QLabel("Valor:")
         
         self.inp_nome = QLineEdit(self)
         self.inp_numero_conta = QLineEdit(self)
-        self.inp_deposito_inicial = QLineEdit(self)
-        self.inp_saque = QLineEdit(self)
+        # self.inp_deposito_inicial = QLineEdit(self)
+        self.inp_saldo = QLineEdit(self)
         self.text_cadastro = QTextBrowser(self)
 
         #Botão
@@ -37,57 +32,53 @@ class Banco(QWidget):
         self.btn_sacar.clicked.connect(self.saque)
         
         
-        
         #Colocando os Widgets
         self.layout.addWidget(self.lbl_nome)
         self.layout.addWidget(self.inp_nome)
         self.layout.addWidget(self.lbl_numero_conta)
         self.layout.addWidget(self.inp_numero_conta)
-        self.layout.addWidget(self.lbl_deposito_inicial)
-        self.layout.addWidget(self.inp_deposito_inicial)
+        self.layout.addWidget(self.lbl_saldo)
+        self.layout.addWidget(self.inp_saldo)
+        # self.layout.addWidget(self.lbl_deposito_inicial)
+        # self.layout.addWidget(self.inp_deposito_inicial)
         self.layout.addWidget(self.btn_cadastro)
         self.layout.addWidget(self.btn_deposito)
         self.layout.addWidget(self.btn_sacar)
-        self.layout.addWidget(self.lbl_saque)
-        self.layout.addWidget(self.inp_saque)
         self.layout.addWidget(self.text_cadastro)
         self.setLayout(self.layout)
+        
 
 
-    def cadastro(self):
 
-
-        self.saldo = float(self.inp_deposito_inicial.text())
+    def cadastro(self,valor):
+        self.valor  = valor 
+        self.valor = float(self.inp_saldo.text())
         self.nome = self.inp_nome.text()
         self.conta = int(self.inp_numero_conta.text())
 
-        self.text_cadastro.append(f"\nNome do Titular: {self.nome}\nNúmero da Conta: {self.conta}\nSaldo: R${self.saldo}\nCadastro Feito com sucesso!\n")
+        self.text_cadastro.append(f"\nNome do Titular: {self.nome}\nNúmero da Conta: {self.conta}\nSaldo: R${self.valor}\nCadastro Feito com sucesso!\n")
 
 
-    def deposito(self):
-
-
-        self.deposito_saldo = float(self.inp_deposito_inicial.text())
-        self.saldo = self.saldo + self.deposito_saldo
-        self.text_cadastro.append(f"\nDeposito Feito!\nNome do Titular: {self.nome}\nNúmero da Conta: {self.conta}\nSaldo: R${self.saldo}\n")
-
-
-    def saque(self):
+    def deposito(self,valor):
         
-        
-        self.deposito_saldo = float(self.inp_deposito_inicial.text())
-        self.saque_saldo = float(self.inp_saque.text())
+        self.valor  = valor 
+        valor = valor + float(self.inp_saldo.text())
+        self.text_cadastro.append(f"\nDeposito Feito!\nNome do Titular: {self.nome}\nNúmero da Conta: {self.conta}\nSaldo: R${valor}\n")
 
-        self.saldo = ((self.deposito_saldo - (5 + self.saque_saldo)))
 
-        if self.saldo < 0:
-            print(self.saldo)
+    def saque(self,valor):
+
+        self.valor = valor 
+        valor = float(self.inp_saldo.text())
+        valor = valor - float(self.inp_saldo.text()) - 5
+
+        if valor < 0:
             self.text_cadastro.append(f"\nSaldo insuficente para saque!\n")
         else:
-            self.text_cadastro.append(f"\nVocê Sacou!\nNome do Titular: {self.nome}\nNúmero da Conta: {self.conta}\nSaldo: R${self.saldo}\n")
+            self.text_cadastro.append(f"\nVocê Sacou!\nNome do Titular: {self.nome}\nNúmero da Conta: {self.conta}\nSaldo: R${valor}\n")
 
             
 appl = QApplication(sys.argv)
 window = Banco() 
 window.show()
-appl.exec()
+appl.exec_()
