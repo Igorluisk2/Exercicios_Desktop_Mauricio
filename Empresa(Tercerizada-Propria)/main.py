7e99065a857a774359098907603a96718471b475
Igor Luís <igorluis_k2@hotmail.com>
2023-08-29 18:05:52 -0400

    update 29/08
    rendeu pouco, acho que é erro de nomenclatura

--- Empresa(Tercerizada-Propria)/main.py
+++ Empresa(Tercerizada-Propria)/main.py
@@ -1,7 +1,7 @@
 from PySide6 import QtCore
 from PySide6.QtCore import QCoreApplication,QPropertyAnimation,QEasingCurve
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import (QApplication, QMainWindow)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox)
 from ui_main import Ui_MainWindow
 import sys
 from database import Data_base
@@ -29,6 +29,7 @@ class Mainwindow(QMainWindow, Ui_MainWindow):
         ######################################
         
         self.btn_cadastrar.clicked.connect(self.cadastrar_empresas)
+        
 
     def leftMenu(self):
         
@@ -50,12 +51,38 @@ class Mainwindow(QMainWindow, Ui_MainWindow):
         db = Data_base()
         db.connect()
         
-        '''fullDataSet = (
-            
-            self.txt_cnpj.(),self.txt_nome.text(),self.txt_logradouro.text(),self.
-        )'''
+        fullDataSet = (
+            self.txt_cnpj.text(), self.txt_nome.text(), self.txt_logradouro.text(), self.txt_numero.text(), 
+            self.txt_complemento.text(), self.txt_bairro.text(), self.txt_municipio.text(), self.txt_uf.text(), 
+            self.txt_cep.text(), self.txt_telefone.text().strip(), self.txt_email.text()
+            )
+        
+        #CADASTRAR NO BD
+        resp = db.register_company(fullDataSet)
+        
+        if resp == "OK":
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox. Information)
+            msg.setWindowTitle("Casdastro Realizado")
+            msg.setText("Cadastro Realizado com sucesso")
+            msg.exec()
+            db.close_connection()
+            return
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Erro")
+            msg.setText("Erro ao cadastrar, verifique se as Informações foram preenchidadas corretamente!") 
+            msg.exec()
+            db.close_connection()
+            return    
        
 if __name__ == "__main__":
+    
+    db = Data_base()
+    db.connect()
+    db.create_table_company()
+    db.close_connection()
     
     app = QApplication(sys.argv)
     window = Mainwindow()
